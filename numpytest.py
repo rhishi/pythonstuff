a0630ef4abd7bedc29707ee265b9a56022057375
Rhishikesh Limaye <rhishi@gmail.com>
2012-08-15 17:08:54 -0700

    Learned a bit of numpy: 2D arrays, matrices, whether a DFT matrix is symmetric.

--- numpytest.py
+++ numpytest.py
@@ -1,7 +1,11 @@
 import numpy as np
+import numpy.fft as fft
+
+def printTypeAndValue(x):
+  print type(x), "\n", x
 
 a = np.arange(10)
-print type(a), a
+printTypeAndValue(a)
 
 def vecadd_python(a, b):
     c = [0] * len(a)
@@ -14,3 +18,32 @@ def vecadd_numpy(a, b, N):
 N = 5000000
 a = range(N)
 b = range(N)
+
+N = 8
+I = np.matrix(np.identity(N))
+
+printTypeAndValue(I)
+printTypeAndValue(I[0])
+printTypeAndValue(np.asarray(I[0]))
+printTypeAndValue(np.squeeze(np.asarray(I[0])))
+printTypeAndValue(np.asarray(I[0])[0])
+printTypeAndValue(np.asarray(I[0]).reshape(-1))
+printTypeAndValue(np.reshape(np.asarray(I[0]), -1))
+
+for r in I:
+  a = np.squeeze(np.asarray(r))
+  print "row", r, "row", a, "fft", fft.fft(a)
+
+DFTMatrix = np.matrix([ fft.fft(np.squeeze(np.asarray(r))) for r in I ])
+
+printTypeAndValue(DFTMatrix)
+
+printTypeAndValue(DFTMatrix.T == DFTMatrix)
+printTypeAndValue((DFTMatrix.T == DFTMatrix).all())
+
+print "Is DFT Matrix exactly equal to its transpose?", ("Yes!" if (DFTMatrix.T == DFTMatrix).all() else "No!") 
+
+printTypeAndValue(DFTMatrix.T - DFTMatrix)
+printTypeAndValue(np.around(DFTMatrix.T - DFTMatrix, 10))
+
+print "Is DFT Matrix nearly equal to its transpose?", ("Yes!" if (abs(DFTMatrix.T - DFTMatrix) < 1e-10).all() else "No!") 
