deb23612ee460fcb4ad2393cf0189f402ffb011d
Rhishikesh Limaye <rhishi@gmail.com>
2012-08-29 15:38:26 -0700

    Extended Gaussian distribution demo to plot integral of PDF using simple trapezoidal integration.

--- gaussian-distribution-ipythondemo.py
+++ gaussian-distribution-ipythondemo.py
@@ -18,6 +18,7 @@ x = np.arange(-5.0, 5.0, 0.05)
 
 # <demo> --- stop ---
 
+plt.subplot(211)
 plt.xlim(-5.0, 5.0)
 plt.ylim(0.0, 1.4)
 plt.xticks(np.arange(-5.0, 5.0, 1.0))
@@ -43,5 +44,44 @@ plt.plot(x, y, 'y')
 
 y = gaussian(x, -2, 0.5)
 plt.plot(x, y, 'g')
+
+# <demo> --- stop ---
+
+def integrate(y, x):
+    n = len(x)
+    r = np.zeros(n)
+    for i in range(1, n):
+        delta = x[i] - x[i-1]
+        r[i] = r[i-1] + 0.5 * (y[i] + y[i-1]) * delta 
+    return r
+
+plt.subplot(212)
+plt.xlim(-5.0, 5.0)
+plt.ylim(0.0, 1.0)
+plt.xticks(np.arange(-5.0, 5.0, 1.0))
+plt.grid(True)
+plt.hold(True)
+
+# <demo> --- stop ---
+
+y = integrate(gaussian(x, 0, 0.1), x)
+plt.plot(x, y, 'b')
+
+# <demo> --- stop ---
+
+y = integrate(gaussian(x, 0, 1), x)
+plt.plot(x, y, 'r')
+
+# <demo> --- stop ---
+
+y = integrate(gaussian(x, 0, 5), x)
+plt.plot(x, y, 'y')
+
+# <demo> --- stop ---
+
+y = integrate(gaussian(x, -2, 0.5), x)
+plt.plot(x, y, 'g')
+
+# <demo> --- stop ---
 
 if not plt.isinteractive(): plt.show()
