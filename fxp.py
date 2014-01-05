64e2733c142872298207a73c633e5400d4dbc48e
Rhishikesh Limaye <rhishi@gmail.com>
2014-01-04 21:04:32 -0800

    Renewed my Python interest: started watching Python Bootcamp, installed Anaconda, and now pythoning with TextMate!  Bye, bye to PyDev & Eclipse.  Updated the integer and fixed-point learnings.  See the added comments.

--- fxp.py
+++ fxp.py
@@ -2,22 +2,38 @@
 
 
 def fxp(number, fractionalBits):
-  scaled = number * 2 ** fractionalBits
-  rounded = int(round(scaled))
-  binaryStr = bin(rounded)
-  binary = binaryStr[2:]
-  wordlength = len(binary)
-  integerBits = wordlength - fractionalBits
-  integer = binary[:integerBits]
-  fraction = binary[integerBits:]
-  return integer + "." + fraction
+    """ returns a fixed point representation of a floating point number
+        rounded up to a given number of fractional bits.
+        
+        The returned value is a string of 1's and 0's and a dot.
+        The substring on the left of the dot gives the binary representation
+        of the integer part of the number.  And the right substring
+        gives the rounded fractional part.
+    """
+    scaled = number * 2 ** fractionalBits
+    rounded = int(round(scaled))
+    binaryStr = bin(rounded)
+    binary = binaryStr[2:]   # remove "0b" prefix  
+    wordlength = len(binary)
+    integerBits = wordlength - fractionalBits
+    if integerBits >= 0:
+        return binary[:integerBits] + "." + binary[integerBits:]
+    else:
+        return "." + "0"*(-integerBits) + binary
 
+data = [ 9.2, 1.0/3, 0.171875, 23.47 ]
 
-data = [ 9.2, 1.0, 0.5, 23.25 ]
-
+# first print a row with our data.
+print "  ",
 for v in data:
-  for f in [8, 10, 16, 24]:
-    print "%30s " % fxp(v, f),
-  print
+    print "%-30f " % v,
+print
+
+# for each fractional bitwidth, print out the fxp strings for the data.
+for f in [8, 9, 12, 15, 16, 24]:
+    print "%2d" % f,
+    for v in data:
+        print "%-30s " % fxp(v, f),
+    print
 
 
--- integers.py
+++ integers.py
@@ -1,13 +1,41 @@
 import sys
 
-print "{0} = {0:x}".format(sys.maxint)
+# In Python 2, there are two integer types: int, long.
 
-# there is no sys.maxlong, because longs are of unlimited size!
+# int is the underlying platform's signed integer type,
+# either 32 or 64 bit, depending on the platform.
+
+# sys.maxint gives the maximum value of int.  It is 2^31-1 or 2^63-1.
+
+print "sys.maxint =  {0} =  {0:x}   {1}".format(sys.maxint, type(sys.maxint))
+
+# There is no sys.minint, but it's simply -sys.maxint-1 as said in Python documentation
+# http://docs.python.org/2/library/stdtypes.html#numeric-types-int-float-long-complex
+
+print "min int    = {0} = {0:x}   {1}".format(-sys.maxint-1, type(-sys.maxint-1))
+
+print
+
+# long is an integer type with unlimited range.  Python automatically
+# switches over from int to long whenever there is overflow.
+
+# That's why, there is no sys.maxlong.
+
+# Python 3 even gets rid of sys.maxint, because it has just single
+# integer type: int.  It actually behaves like 2's long i.e. has unlimited range.
+# 3 has sys.maxsize, which loosely relates to 2's sys.maxint.
+# http://docs.python.org/3.3/whatsnew/3.0.html#integers
+# http://docs.python.org/3/library/stdtypes.html#numeric-types-int-float-complex
+
+# Let's test the automatic switchover from int to long
+# On 64-bit platform, the switchover point is between 2^63-1 and 2^63.
 
 for i in range(1, 130):
-    n = (1 << i) - 1
+    # make 2^i - 1, without spilling beyond i bits.
+    n = (((1 << (i-1)) - 1) << 1) + 1
     print "2**{0:<3} - 1   is   {1:#35x}   {2}".format(i, n, type(n))
     print "2**{0:<3}       is   {1:#35x}   {2}".format(i, n+1, type(n+1))
+print
 
 print -1
 print -1 & 0xFF
